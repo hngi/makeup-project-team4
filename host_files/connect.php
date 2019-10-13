b87b9eb707837e21d8513ce3fcc78aa3ac80b419
Taofeeq-deru <54284970+Taofeeq-deru@users.noreply.github.com>
2019-10-13 16:29:14 +0100

    update connect.php

--- host_files/connect.php
+++ host_files/connect.php
@@ -1,9 +1,9 @@
 <?php
 //database connection
 $db_host = "localhost";
-$db_user = "id11029219_ganymede";
+$db_user = "ganymede";
 $db_password = "ganymede2019";
-$db_name = "id11029219_ganymede_budget_app";
+$db_name = "ganymede_budget_app";
 
 
 $conn = mysqli_connect($db_host, $db_user, $db_password, $db_name);
